207969647122686a3305fb0aae234b50a3455b17
S0ul3r <jakubkulewicz05@gmail.com>
2020-02-17 19:11:21 +0100

    Added netflix

--- cogs/accounts.py
+++ cogs/accounts.py
@@ -32,6 +32,29 @@ class Accounts(commands.Cog):
             print(f"  > User {ctx.author} generated a Spotify Account at time {datetime.datetime.now()}")
             await ctx.send(f"```Username:{User}\nPassword:{PassFixed}```")
 
+    #Print netflix account
+    @commands.command(pass_context=True)
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def netflix(self, ctx):
+        author = ctx.message.author
+        with open('netflix.txt', 'r') as (f):
+            text_spo = f.readlines()
+            while True:
+                randomline = random.choice(text_spo)
+                part = randomline.split(':')
+                User = part[0]
+                Pass = part[1]
+                PassFixed = Pass.rstrip()
+                if len(randomline) == 0:
+                    continue
+                with open('netflix.txt', 'w') as (c):
+                    for line in text_spo:
+                        if line.strip('\n') != f"{User}:{PassFixed}":
+                            c.write(line)
+                break
+            print(f"  > User {ctx.author} generated a Netflix Account at time {datetime.datetime.now()}")
+            await ctx.send(f"```Username:{User}\nPassword:{PassFixed}```")
+
 
 def setup(client):
     client.add_cog(Accounts(client))
